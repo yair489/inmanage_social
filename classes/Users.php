ad53161a545d295a00bdfe4d92918248509ae9f8
yair489 <yairturgeman@gmail.com>
2024-08-13 15:42:31 +0300

    getAll fix

--- classes/Users.php
+++ classes/Users.php
@@ -13,7 +13,7 @@ class Users{
      * username
      * @var string
      */
-    public $user_name;
+    public $user_name;//user_id user_name email active birthday password
     
     /**
      * enail
@@ -43,20 +43,65 @@ class Users{
      */
     public static function getAll($conn)
     {
+        $sql = "SELECT * 
+                From users
+                ORDER BY user_name;";
+
+        $res = $conn->query($sql);
+
+        return $res->fetchAll(PDO::FETCH_ASSOC);
     }
     
     /**
      * 
      */
-    public static function getByID($conn)
+    public static function getByID($conn , $id, $columns = '*')
     {
+        $sql = "SELECT $columns
+                FROM users
+                WHERE id = :id";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'users');
+
+        if ($stmt->execute()) {
+
+            return $stmt->fetch();
+
+        }
     }
     
     /**
-     * 
+     * user_id user_name email active birthday password
      */
     public function update($conn)
     {
+        $sql = "UPDATE users
+                SET user_id = :user_id,
+                    user_name = :user_name,
+                    email = :email
+                    active = :active,
+                    birthday = :birthday,
+                    password = :password
+                WHERE user_id = :id";
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindValue(':id', $this->user_id, PDO::PARAM_INT);
+        $stmt->bindValue(':user_name', $this->user_name, PDO::PARAM_STR);
+        $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
+        $stmt->bindValue(':active', $this->active, PDO::PARAM_INT);
+        $stmt->bindValue(':birthday', $this->birthday, PDO::PARAM_STR);
+        $stmt->bindValue(':password', $this->password, PDO::PARAM_STR);
+
+        if ($this->published_at == '') {
+            $stmt->bindValue(':published_at', null, PDO::PARAM_NULL);
+        } else {
+            $stmt->bindValue(':published_at', $this->published_at, PDO::PARAM_STR);
+        }
+
+        return $stmt->execute();
     }
     
     /**
@@ -64,6 +109,14 @@ class Users{
      */
     public function delete($conn)
     {
+        $sql = "DELETE FROM article
+                WHERE id = :id";
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindValue(':id', $this->id, PDO::PARAM_INT);
+
+        return $stmt->execute();
     }
     
     /**
@@ -71,5 +124,23 @@ class Users{
      */
     public function create($conn)
     {
+        $sql = "INSERT INTO article (title, content, published_at)
+                    VALUES (:title, :content, :published_at)";
+
+            $stmt = $conn->prepare($sql);
+
+            $stmt->bindValue(':title', $this->title, PDO::PARAM_STR);
+            $stmt->bindValue(':content', $this->content, PDO::PARAM_STR);
+
+            if ($this->published_at == '') {
+                $stmt->bindValue(':published_at', null, PDO::PARAM_NULL);
+            } else {
+                $stmt->bindValue(':published_at', $this->published_at, PDO::PARAM_STR);
+            }
+
+            if ($stmt->execute()) {
+                $this->id = $conn->lastInsertId();
+                return true;
+            }
     }
 }
